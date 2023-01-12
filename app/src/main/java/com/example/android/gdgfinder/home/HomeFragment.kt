cc8102fb2d19d8a6e4eeb73269d62555a8cba6a0
nasimkjiniya <nasimkjiniya@gmail.com>
2023-01-12 14:19:59 -0800

    Added floating button to the view and used material design pattern

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -15,14 +22,27 @@ class HomeFragment : Fragment() {
     }
 
     private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        binding = HomeFragmentBinding.inflate(inflater)
+
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel=viewModel
+
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer {shouldNavigate->
+            if(shouldNavigate)
+            {
+                val navController = binding.root.findNavController()
+                navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                viewModel.onNavigatedToSearch()
+            }
+        })
+
+        return binding.root
     }
 }
